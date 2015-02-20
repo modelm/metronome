8256f59b5f8d6b6dce5013134b1fddee2222f025
Ryan Williams <ryan@accustack.com>
2015-02-20 17:31:30 -0500

    first

---
+++ js/metronome.js
@@ -0,0 +1,94 @@
+var Metronome = {
+	interval: null,
+
+	beat: 0,
+
+	time: parseInt(document.getElementById('time').value),
+
+	tick: function() {
+		var sound = document.getElementById('tick');
+
+		if (Metronome.time === 0) {
+			// no time signature, just play the tick and increment the beat indefinitely
+			Metronome.beat++;
+		} else if ((Metronome.beat < Metronome.time) && (Metronome.beat % Metronome.time > 0)) {
+			Metronome.beat++;
+		} else {
+			Metronome.beat = 1;
+			sound = document.getElementById('tick-downbeat');
+		}
+
+		document.getElementById('visual-target').innerHTML = Metronome.beat;
+
+		if (!document.getElementById('mute').checked) {
+			sound.play();
+		}
+
+		console.log('tick');
+	},
+
+	start: function() {
+		var bpm = document.getElementById('bpm').value;
+		var beepInterval = (60 / bpm) * 1000;
+		if(bpm > 0) {
+			if (Metronome.interval !== null) window.clearInterval(Metronome.interval);
+			Metronome.interval = window.setInterval(Metronome.tick, beepInterval);
+			Metronome.tick(); // unless we call this now, we wait until beepInterval has passed before the first tick comes
+			document.getElementById('start').style.display = 'none';
+			document.getElementById('stop').style.display = '';
+			console.log('metronome started');
+		} else {
+			console.log('bpm must be positive', bpm); // TODO tell user
+		}
+	},
+
+	stop: function() {
+		window.clearInterval(Metronome.interval);
+		Metronome.interval = null;
+		Metronome.beat = 0;
+		document.getElementById('visual-target').innerHTML = '';
+		document.getElementById('start').style.display = '';
+		document.getElementById('stop').style.display = 'none';
+		console.log('metronome stopped');
+	},
+
+	restart: function() {
+		Metronome.stop();
+		Metronome.start();
+	},
+
+	addToBpm: function(difference) {
+		var bpmInput = document.getElementById('bpm');
+		bpmInput.value = parseInt(bpmInput.value) + difference;
+		if (Metronome.interval !== null) Metronome.start();
+	},
+
+	init: function() {
+		document.getElementById('stop').style.display = 'none';
+
+		document.getElementById('bpm').onkeyup = Metronome.restart;
+		document.getElementById('start').onclick = Metronome.start;
+		document.getElementById('stop').onclick = Metronome.stop;
+
+		document.getElementById('time').onkeyup = function(){
+			// TODO error handling for non-integers
+			// TODO support input like "2+3" for 5/4
+			Metronome.time = parseInt(this.value);
+		}
+
+		document.getElementById('minus10').onclick = function(){
+			Metronome.addToBpm(-10);
+		}
+		document.getElementById('minus1').onclick = function(){
+			Metronome.addToBpm(-1);
+		}
+		document.getElementById('plus10').onclick = function(){
+			Metronome.addToBpm(10);
+		}
+		document.getElementById('plus1').onclick = function(){
+			Metronome.addToBpm(1);
+		}
+	}
+}
+
+Metronome.init();

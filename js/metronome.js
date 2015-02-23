e118d2e04ac3bacbbb001d515ad1a25163b3af31
Ryan Williams <ryan@accustack.com>
2015-02-23 12:25:15 -0500

    style updates, keyboard shortcuts improvements/extensions, localStorage remembers tempo/time

--- js/metronome.js
+++ js/metronome.js
@@ -1,5 +1,5 @@
 /**
- * depends on Mousetrap for keyboard shortcuts: http://cdn.craig.is/js/mousetrap/mousetrap.min.js
+ * depends on Mousetrap for keyboard shortcuts: http://craig.is/killing/mice
  */
 
 var Metronome = {
@@ -16,6 +16,11 @@ var Metronome = {
 	taps: [], // will contain times when the tap button was clicked (shift()ed every tap after the second one)
 
 	context: new (window.AudioContext || window.webkitAudioContext), // audio context in which to create and use the tone generator
+
+	defaults: {
+		tempo: 120,
+		time: 0,
+	},
 
 	frequencies: {
 		DOWNBEAT: 4000,
@@ -52,24 +57,22 @@ var Metronome = {
 		document.getElementById('visual-target').innerHTML = Metronome.beat;
 
 		// audio tick
-		if (!document.getElementById('mute').checked) {
-			osc.connect(Metronome.context.destination);
-			osc.start(Metronome.context.currentTime);
-			osc.stop(Metronome.context.currentTime + 0.0005);
-		}
+		osc.connect(Metronome.context.destination);
+		osc.start(Metronome.context.currentTime);
+		osc.stop(Metronome.context.currentTime + 0.0005);
 
 		console.log('metronome tick');
 	},
 
 	start: function() {
-		var tempo = parseInt(Metronome.tempoInput.value) || 120;
-		var beepInterval = (60 / tempo) * 1000;
+		var tempo = parseInt(Metronome.tempoInput.value) || Metronome.defaults.tempo;
+		var tickInterval = (60 / tempo) * 1000;
 
 		Metronome.tempoInput.value = tempo; // just in case a bad value made it in here somehow, set it to what we're actually using
 
 		if (tempo > 0) {
 			if (Metronome.interval !== null) window.clearInterval(Metronome.interval);
-			Metronome.interval = window.setInterval(Metronome.tick, beepInterval);
+			Metronome.interval = window.setInterval(Metronome.tick, tickInterval);
 			document.getElementById('start').style.display = 'none';
 			document.getElementById('stop').style.display = '';
 			console.log('metronome started');
@@ -89,7 +92,7 @@ var Metronome = {
 	},
 
 	restart: function() {
-		Metronome.stop();
+		//Metronome.stop();
 		Metronome.start();
 	},
 
@@ -99,17 +102,14 @@ var Metronome = {
 	},
 
 	parseTime: function() {
-		Metronome.timeInput.value = Metronome.timeInput.value.replace(/^[^1-9\+]/g, ''); // remove characters which are not numbers or '+'
-
+		Metronome.timeInput.value = Metronome.timeInput.value.replace(/[^1-9\+]/g, ''); // remove characters which are not numbers or '+'
 		Metronome.timeInput.value = Metronome.timeInput.value.replace(/\++/g, '+'); // remove extraneous instances of '+'
 
 		if (!/^\+/.test(Metronome.timeInput.value) && !/\+$/.test(Metronome.timeInput.value)) { // ignore input beginning or ending with '+'
-			if (!Metronome.timeInput.value.length) Metronome.timeInput.value = 0;
+			if (!Metronome.timeInput.value.length) Metronome.timeInput.value = Metronome.defaults.time;
 
 			Metronome.time = Metronome.timeInput.value;
-
 			Metronome.groupings = Metronome.time.split('+');
-
 			Metronome.strongBeats = [1];
 
 			for (var i = 0; i < Metronome.groupings.length - 1; i++) {
@@ -120,158 +120,124 @@ var Metronome = {
 				}
 			}
 
+			localStorage.setItem('metronome.time', Metronome.time);
+
 			console.log('metronome parsed time: ', Metronome.time, ' groupings: ', Metronome.groupings, ' strong beats: ', Metronome.strongBeats);
 		}
 	},
 
+	parseTempo: function() {
+		Metronome.tempoInput.value = Metronome.tempoInput.value.replace(/[^0-9]/g, ''); // remove non-numeric characters
+
+		localStorage.setItem('metronome.tempo', Metronome.tempoInput.value);
+
+		if (Metronome.interval) Metronome.restart();
+	},
+
+	handleTap: function() {
+		Metronome.taps.push(Metronome.context.currentTime);
+
+		if (Metronome.taps.length > 2) {
+			Metronome.taps.shift();
+		}
+
+		if (Metronome.taps.length > 1) {
+			document.getElementById('tempo').value = (function() {
+				var secondsSinceLastTap = Metronome.taps[1] - Metronome.taps[0];
+				console.log('metronome seconds since last tap: ', secondsSinceLastTap);
+				return Math.floor(60 / secondsSinceLastTap);
+			})();
+		}
+
+		if (Metronome.interval) Metronome.start();
+		console.log('metronome taps: ', Metronome.taps);
+	},
+
+	showHelp: function() {
+		var lines = [
+			'beats per minute can be either of the following:\n',
+			'• "0" (no grouping)',
+			'• one or more numbers separated by "+", e.g. "4", "2+3", "3+2+2"',
+			'\n\n',
+			'keyboard shortcuts:\n',
+			'• space - start/stop',
+			'• up - increment tempo by 1',
+			'• down - decrement tempo by 1',
+			'• left - decrement tempo by 10',
+			'• right - increment tempo by 10',
+			'• . - tap tempo',
+			'• ? - show this help',
+		];
+
+		alert(lines.join('\n'));
+
+		return false;
+	},
+
 	bindControls: function() {
-		var decrementTempoBig = function() {
-			Metronome.addToTempo(-10);
-		}
-		var decrementTempoSmall = function() {
-			Metronome.addToTempo(-1);
-		}
-		var incrementTempoSmall = function() {
+		// keyboard shortcuts
+		Mousetrap.bindGlobal('?', Metronome.showHelp);
+		Mousetrap.bindGlobal('.', Metronome.handleTap);
+		Mousetrap.bindGlobal('t', function(e) {
+			e.preventDefault();
+			Metronome.tempoInput.focus();
+		});
+		Mousetrap.bindGlobal('b', function(e) {
+			e.preventDefault();
+			Metronome.timeInput.focus();
+		});
+		Mousetrap.bindGlobal('up', function() {
 			Metronome.addToTempo(1);
-		}
-		var incrementTempoBig = function() {
+		});
+		Mousetrap.bindGlobal('down', function() {
+			Metronome.addToTempo(-1);
+		});
+		Mousetrap.bindGlobal('right', function() {
 			Metronome.addToTempo(10);
-		}
-		var startStop = function() { // start if stopped; stop if started
+		});
+		Mousetrap.bindGlobal('left', function() {
+			Metronome.addToTempo(-10);
+		});
+		Mousetrap.bindGlobal('space', function() { // start if stopped; stop if started
 			if (Metronome.interval) {
 				Metronome.stop();
 			} else {
 				Metronome.start();
 			}
-		}
-		var inputs = document.getElementsByTagName('input');
-		var keydownHandler = function(e) {
-			switch (e.which) {
-				case 32: // space
-					startStop();
-					return false;
-				case 37: // left arrow
-					decrementTempoBig();
-					return false;
-				case 39: // right arrow
-					incrementTempoBig();
-					return false;
-				case 38: // up arrow
-					incrementTempoSmall();
-					return false;
-				case 40: // down arrow
-					decrementTempoSmall();
-					return false;
-			}
-		}
-
-		// ensure control keys still work when inputs are focused
-		for (var i = 0; i < inputs.length; i++) {
-			inputs[i].onkeydown = keydownHandler;
-		}
-
-		// start/stop
-		document.getElementById('start').onclick = Metronome.start;
-		document.getElementById('stop').onclick = Metronome.stop;
-		Mousetrap.bind('space', function() {
-			startStop();
-			return false;
 		});
 
 		// tempo / beats per minute
-		Metronome.tempoInput.onkeyup = function() {
-			if (Metronome.interval) {
-				Metronome.restart();
-			}
-		}
-		Metronome.tempoInput.onchange = function() {
-			if (this.value === '' || parseInt(this.value) < 1) {
-				this.value = 120;
-			}
-		}
+		Metronome.tempoInput.onkeyup = Metronome.parseTempo;
 
 		// time / beats per measure
-		Metronome.timeInput.onkeydown = function(e) {
-			if (this.value === '0') {
-				this.value = '';
-			}
-			return keydownHandler(e);
-		}
-		Metronome.timeInput.onkeyup = function() {
-			Metronome.parseTime();
-		}
-		Metronome.timeInput.onchange = function() {
-			if (this.value === '') {
-				this.value = 0;
-			}
-		}
+		Metronome.timeInput.onkeyup = Metronome.parseTime;
 
-		// increment/decrement
-		document.getElementById('minus10').onclick = decrementTempoBig;
-		Mousetrap.bind('left', decrementTempoBig);
+		// start/stop buttons
+		document.getElementById('start').onclick = Metronome.start;
+		document.getElementById('stop').onclick = Metronome.stop;
 
-		document.getElementById('plus10').onclick = incrementTempoBig;
-		Mousetrap.bind('right', incrementTempoBig);
+		// increment/decrement buttons
+		document.getElementById('minus10').onclick = Mousetrap.trigger('left');
+		document.getElementById('plus10').onclick = Mousetrap.trigger('right');
+		document.getElementById('minus1').onclick = Mousetrap.trigger('down');
+		document.getElementById('plus1').onclick = Mousetrap.trigger('up');
 
-		document.getElementById('minus1').onclick = decrementTempoSmall;
-		Mousetrap.bind('down', decrementTempoSmall);
+		// tap tempo button
+		document.getElementById('tap').onclick = Metronome.handleTap;
 
-		document.getElementById('plus1').onclick = incrementTempoSmall;
-		Mousetrap.bind('up', incrementTempoSmall);
+		// help anchor
+		document.getElementById('show-help').onclick = Metronome.showHelp;
 
-		// small window popout
-		document.getElementById('popout').onclick = function(){
+		// small window popout button
+		document.getElementById('popout').onclick = function() {
 			Metronome.stop();
 			window.open('index.html', '_blank', 'width=320,height=480,resizable=no,scrollbars=no,menubar=no,location=no,status=no,toolbar=no');
 		}
-
-		// tap tempo
-		document.getElementById('tap').onclick = function() {
-			Metronome.taps.push(Metronome.context.currentTime);
-
-			if (Metronome.taps.length > 2) {
-				Metronome.taps.shift();
-			}
-
-			if (Metronome.taps.length > 1) {
-				document.getElementById('tempo').value = (function() {
-					var secondsSinceLastTap = Metronome.taps[1] - Metronome.taps[0];
-
-					console.log('metronome seconds since last tap: ', secondsSinceLastTap);
-
-					return Math.floor(60 / secondsSinceLastTap);
-				})();
-			}
-
-			if (Metronome.interval) Metronome.start();
-
-			console.log('metronome taps: ', Metronome.taps);
-		}
-
-		// help
-		document.getElementById('show-help').onclick = function() {
-			var lines = [];
-
-			lines.push('tempo can be any positive integer');
-
-			lines.push('\n\nbeats per minute can be either of the following:\n');
-			lines.push('• "0" (no grouping)');
-			lines.push('• one or more numbers separated by "+", e.g. "4", "2+3", "3+2+2"');
-
-			lines.push('\n\nkeyboard shortcuts:\n');
-			lines.push('• space: start/stop');
-			lines.push('• up arrow: increment tempo by 1');
-			lines.push('• down arrow: decrement tempo by 1');
-			lines.push('• left arrow: decrement tempo by 10');
-			lines.push('• right arrow: increment tempo by 10');
-
-			alert(lines.join('\n'));
-
-			return false;
-		};
 	},
 
 	init: function() {
+		Metronome.tempoInput.value = localStorage.getItem('metronome.tempo');
+		Metronome.timeInput.value = localStorage.getItem('metronome.time');
 		Metronome.parseTime();
 		Metronome.bindControls();
 	}

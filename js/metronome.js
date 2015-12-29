f082f3085e0e854e8bbe440088421bf41d3cee74
Ryan Williams <ryrowi@gmail.com>
2015-12-28 19:55:00 -0500

    refactoring & optimization

--- js/metronome.js
+++ js/metronome.js
@@ -1,5 +1,6 @@
 /* jshint evil:true */
 /* globals Mousetrap */
+/* exported Metronome */
 
 var Metronome = {
 	interval: null, // used to store the result of setInterval() while ticking
@@ -16,6 +17,8 @@ var Metronome = {
 
 	tuner: false, // keeps track of the oscillator used for the tuning tone generator (false while tuner is off)
 
+	debug: true, // enable console logging
+
 	settings: {
 		tempo: 120,
 		time: 0,
@@ -25,8 +28,7 @@ var Metronome = {
 			strong: 2000,
 			weak: 1500,
 			tuner: 440
-		},
-		debug: true
+		}
 	},
 
 	inputs: {
@@ -70,7 +72,7 @@ var Metronome = {
 		osc.start(Metronome.context.currentTime);
 		osc.stop(Metronome.context.currentTime + Metronome.settings.duration);
 
-		if (Metronome.settings.debug) {
+		if (Metronome.debug) {
 			console.log('tick');
 		}
 	},
@@ -99,6 +101,67 @@ var Metronome = {
 		document.getElementById('stop').style.display = 'none';
 	},
 
+	startTuner: function () {
+		document.getElementById('start-tuner').style.display = 'none';
+		document.getElementById('stop-tuner').style.display = '';
+		Metronome.tuner = Metronome.context.createOscillator();
+		Metronome.tuner.frequency.value = Metronome.settings.frequencies.tuner;
+		Metronome.tuner.connect(Metronome.context.destination);
+		Metronome.tuner.start();
+		return false;
+	},
+
+	stopTuner: function () {
+		document.getElementById('start-tuner').style.display = '';
+		document.getElementById('stop-tuner').style.display = 'none';
+		Metronome.tuner.stop();
+		Metronome.tuner = false;
+		return false;
+	},
+
+	handleTap: function () {
+		Metronome.taps.push(Metronome.context.currentTime);
+
+		if (Metronome.taps.length > 2) {
+			Metronome.taps.shift();
+		}
+
+		if (Metronome.taps.length > 1) {
+			Metronome.inputs.tempo.value = Math.floor(60 / (Metronome.taps[1] - Metronome.taps[0]));
+		}
+
+		Metronome.save();
+
+		if (Metronome.debug) {
+			console.log('taps: ', Metronome.taps);
+		}
+	},
+
+	randomizeInputValue: function (input) {
+		var getRandomInt = function (min, max) {
+			return Math.floor(Math.random() * (max - min + 1)) + min;
+		};
+
+		switch (input) {
+			case Metronome.inputs.tempo:
+				console.log('tempo');
+				Metronome.inputs.tempo.value = getRandomInt(25, 960);
+				break;
+			case Metronome.inputs.time:
+				console.log('time');
+				Metronome.inputs.time.value = (function () {
+					var groups = [];
+					for (var i = 0; i < getRandomInt(0, 3); i += 1) {
+						groups.push(getRandomInt(2, 4));
+					}
+					return groups.join('+');
+				})();
+				break;
+		}
+
+		Metronome.save();
+	},
+
 	parseTime: function () {
 		Metronome.inputs.time.value = Metronome.inputs.time.value.replace(/[^1-9\+]/g, ''); // remove characters which are not numbers or '+'
 		Metronome.inputs.time.value = Metronome.inputs.time.value.replace(/\++/g, '+'); // remove extraneous instances of '+'
@@ -119,9 +182,9 @@ var Metronome = {
 						Metronome.strongBeats.push(Metronome.groups[i]);
 					}
 				}
-			});
+			}, this);
 
-			if (Metronome.settings.debug) {
+			if (Metronome.debug) {
 				console.log('time', Metronome.settings.time);
 				console.log('groups', Metronome.groups);
 				console.log('strong beats', Metronome.strongBeats);
@@ -136,9 +199,12 @@ var Metronome = {
 				Metronome.start();
 			}
 		} else {
-			if (Metronome.settings.debug) {
+			if (Metronome.debug) {
 				console.warn('tempo must be positive');
 			}
+		}
+		if (Metronome.debug) {
+			console.log('tempo', Metronome.settings.tempo);
 		}
 	},
 
@@ -154,52 +220,24 @@ var Metronome = {
 		}
 	},
 
-	handleTap: function () {
-		Metronome.taps.push(Metronome.context.currentTime);
-
-		if (Metronome.taps.length > 2) {
-			Metronome.taps.shift();
-		}
-
-		if (Metronome.taps.length > 1) {
-			Metronome.inputs.tempo.value = Math.floor(60 / (Metronome.taps[1] - Metronome.taps[0]));
-		}
-
-		Metronome.save();
-
-		if (Metronome.settings.debug) {
-			console.log('taps: ', Metronome.taps);
-		}
-	},
-
-	showHelp: function () {
-		alert([
-			'beats per measure can be either of the following:\n',
-			'• "" or "0" (no groups)',
-			'• one or more numbers separated by "+" e.g. "4", "2+3", "3+2+2"',
-			'\n\n',
-			'keyboard shortcuts:\n',
-			'• space - start/stop',
-			'• up - increment tempo by 1',
-			'• down - decrement tempo by 1',
-			'• left - decrement tempo by 10',
-			'• right - increment tempo by 10',
-			'• n - halve tempo',
-			'• m - double tempo',
-			'• j - third tempo',
-			'• k - triple tempo',
-			'• . - tap',
-			'• b - focus beats per measure',
-			'• t - focus tempo',
-			'• ? - show this help'
-		].join('\n'));
-
-		return false;
-	},
-
-	// depends on Mousetrap for keyboard shortcuts: http://craig.is/killing/mice
 	bindControls: function () {
-		// keyboard shortcuts
+		var eachRecursive = function (obj, callback) {
+			for (var k in obj) {
+				if(obj.hasOwnProperty(k)) {
+					if (obj[k].constructor === Object) { // true for Object literals, not HTMLElements
+						eachRecursive(obj[k], callback);
+					} else {
+						callback(obj[k]);
+					}
+				}
+			}
+		};
+
+		eachRecursive(Metronome.inputs, function (input) {
+			input.onkeyup = Metronome.save; // for typing directly into the input
+			input.onchange = Metronome.save; // for type=number controls & mobile
+		});
+
 		Mousetrap.bindGlobal('?', Metronome.showHelp);
 		Mousetrap.bindGlobal('.', function (e) {
 			e.preventDefault();
@@ -254,11 +292,11 @@ var Metronome = {
 			Metronome.save();
 		});
 
-		// start/stop buttons
+		// start/stop
 		document.getElementById('start').onclick = Metronome.start;
 		document.getElementById('stop').onclick = Metronome.stop;
 
-		// increment/decrement buttons
+		// tempo manipulation
 		document.getElementById('third').onclick = function () {
 			Mousetrap.trigger('j');
 		};
@@ -284,88 +322,36 @@ var Metronome = {
 			Mousetrap.trigger('k');
 		};
 
-		// tap tempo button
+		// tap tempo
 		document.getElementById('tap').onclick = Metronome.handleTap;
 
-		// help anchor
+		// help
 		document.getElementById('show-help').onclick = Metronome.showHelp;
 
-		// small window popout button
+		// small window popout
 		document.getElementById('popout').onclick = function () {
 			Metronome.stop();
 			window.open('index.html', '_blank', 'width=320,height=400,resizable=no,scrollbars=no,menubar=no,location=no,status=no,toolbar=no');
 		};
 
 		// randomize
-		document.getElementById('randomize-tempo').onclick = Metronome.randomizeTempo;
-		document.getElementById('randomize-time').onclick = Metronome.randomizeTime;
+		document.getElementById('randomize-tempo').onclick = function () {
+			Metronome.randomizeInputValue(Metronome.inputs.tempo);
+		};
+		document.getElementById('randomize-time').onclick = function () {
+			Metronome.randomizeInputValue(Metronome.inputs.time);
+		};
 		document.getElementById('randomize-both').onclick = function () {
-			Metronome.randomizeTempo();
-			Metronome.randomizeTime();
+			Metronome.randomizeInputValue(Metronome.inputs.tempo);
+			Metronome.randomizeInputValue(Metronome.inputs.time);
 		};
 
-		// tuner start/stop buttons
+		// tuner start/stop
 		document.getElementById('start-tuner').onclick = Metronome.startTuner;
 		document.getElementById('stop-tuner').onclick = Metronome.stopTuner;
 
-		// settings change handlers. onkeyup for typing directly into the input, onchange for using the type=number controls & mobile
-		for (var i in Metronome.inputs) {
-			if(Metronome.inputs.hasOwnProperty(i)) {
-				Metronome.inputs[i].onkeyup = Metronome.save;
-				Metronome.inputs[i].onchange = Metronome.save;
-
-				// TODO is there a cleaner way to do this?
-				if (i === 'frequencies') {
-					for (var j in i) {
-						if(Metronome.inputs.frequencies.hasOwnProperty(j)) {
-							Metronome.inputs.frequencies[j].onkeyup = Metronome.save;
-							Metronome.inputs.frequencies[j].onchange = Metronome.save;
-						}
-					}
-				}
-			}
-		}
-
 		// reset to defaults
 		document.getElementById('reset').onclick = Metronome.reset;
-	},
-
-	getRandomInt: function (min, max) {
-		return Math.floor(Math.random() * (max - min + 1)) + min;
-	},
-
-	randomizeTempo: function () {
-		Metronome.inputs.tempo.value = Metronome.getRandomInt(25, 960);
-		Metronome.save();
-	},
-
-	randomizeTime: function () {
-		var groups = [];
-
-		for (var i = 0; i < Metronome.getRandomInt(0, 3); i += 1) {
-			groups.push(Metronome.getRandomInt(2, 4));
-		}
-
-		Metronome.inputs.time.value = groups.join('+');
-		Metronome.save();
-	},
-
-	startTuner: function () {
-		document.getElementById('start-tuner').style.display = 'none';
-		document.getElementById('stop-tuner').style.display = '';
-		Metronome.tuner = Metronome.context.createOscillator();
-		Metronome.tuner.frequency.value = Metronome.settings.frequencies.tuner;
-		Metronome.tuner.connect(Metronome.context.destination);
-		Metronome.tuner.start();
-		return false;
-	},
-
-	stopTuner: function () {
-		document.getElementById('start-tuner').style.display = '';
-		document.getElementById('stop-tuner').style.display = 'none';
-		Metronome.tuner.stop();
-		Metronome.tuner = false;
-		return false;
 	},
 
 	save: function () {
@@ -374,20 +360,20 @@ var Metronome = {
 		Metronome.parseFrequencies();
 		Metronome.settings.duration = parseFloat(Metronome.inputs.duration.value);
 
-		localStorage.setItem('Metronome', JSON.stringify(Metronome));
+		localStorage.setItem('Metronome.settings', JSON.stringify(Metronome.settings));
 
-		if (Metronome.settings.debug) {
-			console.log('saved');
+		if (Metronome.debug) {
+			console.log('saved', Metronome.settings);
 		}
 	},
 
 	load: function () {
-		var savedMetronome = JSON.parse(localStorage.getItem('Metronome'));
+		var savedSettings = JSON.parse(localStorage.getItem('Metronome.settings'));
 
-		if (savedMetronome && savedMetronome.settings) {
-			for (var i in savedMetronome.settings) {
+		if (savedSettings) {
+			for (var i in savedSettings) {
 				if(Metronome.settings.hasOwnProperty(i)) {
-					Metronome.settings[i] = savedMetronome.settings[i];
+					Metronome.settings[i] = savedSettings[i];
 				}
 			}
 		}
@@ -407,20 +393,44 @@ var Metronome = {
 		Metronome.parseFrequencies();
 		Metronome.settings.duration = parseFloat(Metronome.inputs.duration.value);
 
-
-		if (Metronome.settings.debug) {
-			console.log('loaded');
+		if (Metronome.debug) {
+			console.log('loaded', Metronome.settings);
 		}
 	},
 
 	reset: function () {
-		localStorage.removeItem('Metronome');
+		localStorage.removeItem('Metronome.settings');
 		location.reload();
 		return false;
 	},
 
+	showHelp: function () {
+		alert([
+				'beats per measure can be either of the following:\n',
+				'• "" or "0" (no groups)',
+				'• one or more numbers separated by "+" e.g. "4", "2+3", "3+2+2"',
+				'\n\n',
+				'keyboard shortcuts:\n',
+				'• space - start/stop',
+				'• up - increment tempo by 1',
+				'• down - decrement tempo by 1',
+				'• left - decrement tempo by 10',
+				'• right - increment tempo by 10',
+				'• n - halve tempo',
+				'• m - double tempo',
+				'• j - third tempo',
+				'• k - triple tempo',
+				'• . - tap',
+				'• b - focus beats per measure',
+				'• t - focus tempo',
+				'• ? - show this help'
+		].join('\n'));
+
+		return false;
+	},
+
 	init: function () {
-		Metronome.load();
 		Metronome.bindControls();
+		Metronome.load();
 	}
 };
